3b53f3e5f50e440a15dbc9342458c64326daadee
Rajaf Dereje <rajafgudeta@gmail.com>
2025-08-24 17:46:55 +0300

    add transition animation between pages

--- Flutter/go_router practice/app/lib/main.dart
+++ Flutter/go_router practice/app/lib/main.dart
@@ -17,15 +17,30 @@ Widget myApp() {
       GoRoute(
         name: RouteName.dashboard,
         path: '/dashboard',
-        builder: (context, state) => DashboardPage(),
+        pageBuilder: (context, state) => CustomTransitionPage(
+          key: state.pageKey,
+          child: DashboardPage(),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) =>
+              FadeTransition(opacity: animation, child: child),
+        ),
+        // builder: (context, state) => DashboardPage(),
       ),
       GoRoute(
         name: RouteName.profile,
         path: '/profile',
-        builder: (context, state) { //? use the state to get passed values
+        pageBuilder: (context, state) {
           final String? name = state.uri.queryParameters['name'];
           final user = state.extra as User; //* cast to User class
-          return ProfilePage(name: name, user: user,);
+
+          return CustomTransitionPage(
+            key: state.pageKey, //? unique key to navigate the stack
+            child: ProfilePage(user: user, name: name),
+            transitionsBuilder:
+                (context, animation, secondaryAnimation, child) {
+                  //? define the animation here
+                  return FadeTransition(opacity: animation, child: child);
+                },
+          );
         },
       ),
     ],
