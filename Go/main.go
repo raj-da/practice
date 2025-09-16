2021eecf83edc1fc4adcb308d189f88f31e4b84a
Rajaf Dereje <rajafgudeta@gmail.com>
2025-09-16 18:19:48 +0300

    standard library

--- Go/main.go
+++ Go/main.go
@@ -1,7 +1,30 @@
-package main;
+package main
 
-import "fmt";
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
 
 func main()  {
-	fmt.Println("Hello World form go");
+	greeting := "hello there friends";
+
+	fmt.Println(strings.Contains(greeting, "hello"));
+	fmt.Println(strings.ReplaceAll(greeting, "hello", "hi"));
+	fmt.Println(strings.ToUpper(greeting));
+	fmt.Println(strings.Index(greeting, "ll"));
+	fmt.Println(strings.Split(greeting, " "));
+
+	ages := []int{45, 20, 35, 30, 75, 60, 50, 25};
+
+	sort.Ints(ages)
+	fmt.Println(ages);
+
+	index := sort.SearchInts(ages, 30);
+	fmt.Println(index)
+
+	names := []string{"yoshi", "mario", "bowser", "luigi"};
+
+	sort.Strings(names);
+	fmt.Println(names);
 }
