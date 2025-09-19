c3bbfb230045b5be79b9435be72929e7ee00759f
Rajaf Dereje <rajafgudeta@gmail.com>
2025-09-19 13:16:13 +0300

    add loop

--- Go/main.go
+++ Go/main.go
@@ -3,5 +3,22 @@ package main;
 import "fmt";
 
 func main()  {
-	fmt.Println("Hello World form go");
+	x := 0;
+
+	//* while loop
+	for x < 5 {
+		fmt.Println("value of x is:", x);
+		x++;
+	}
+
+	//* for loop
+	for i := 0; i < 5; i++ {
+		fmt.Println("value of i is:", i);
+	}
+
+	names := []string{"mario", "luigi", "yoshi", "peach"};
+	//* for in loop
+	for index, value := range names {
+		fmt.Printf("the position at index %v is %v \n", index, value);
+	}
 }
