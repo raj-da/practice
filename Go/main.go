1f5bac852859cb058f930fd7736423ee8cb248c4
Rajaf Dereje <rajafgudeta@gmail.com>
2025-09-19 16:08:05 +0300

    pointer

--- Go/main.go
+++ Go/main.go
@@ -2,18 +2,17 @@ package main
 
 import "fmt"
 
+func updateName(name *string) {
+	*name = "wedge"
+}
+
 func main() {
+	name := "tifa"
 
-	menu := map[string]float64{"soup": 499, "pie": 799, "salad": 6.99, "tofee pudding": 5.55}
+	var address *string = &name; 
+	fmt.Println("memory address of name is:", &address);
+	fmt.Println("value at memory address is", *address)
 
-	fmt.Println(menu)
-	fmt.Println(menu["pie"])
-
-	//* looping maps
-	for key, value := range menu {
-		fmt.Println(key, "-", value)
-	}
-
-	//* update a map
-	menu["soup"] = 4.99
+	updateName(address)
+	fmt.Println("name after update:", name)
 }
