36587d803c5a8ea685225c4846dca387fdc8d789
Rajaf Dereje <rajafgudeta@gmail.com>
2025-09-19 14:20:02 +0300

    package

--- Go/main.go
+++ Go/main.go
@@ -1,39 +1,11 @@
 package main
 
-import (
-	"fmt";
-	"math"
-)
+import "fmt";
 
-func sayGreeting(name string)  {
-	fmt.Printf("Good morning %v \n", name);
-}
+func main(){
+	sayHello("Rajaf");
 
-func sayBye(name string)  {
-	fmt.Printf("Good bye %v \n", name);
-}
-
-//* passing a slice and fuction
-func cycleNames (n []string, f func(string)) {
-	for _, name := range n {
-		f(name);
+	for _, v := range points {
+		fmt.Println(v);
 	}
-}
-
-//* returning a value from a function
-func circleArea(r float64) float64{
-	area := math.Pi * r * r;
-	return area
-}
-
-func main()  {
-	sayGreeting("mario");
-	sayBye("lugi");
-
-	cycleNames([]string{"cloud", "tifa", "barret"}, sayGreeting);
-
-	a1 := circleArea(10.5);
-	a2 := circleArea(15);
-
-	fmt.Printf("circle 1 is %0.3f and circle 2 is %0.3f \n", a1, a2);
 }
