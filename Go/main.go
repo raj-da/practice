398798c125dbd7009ffa27311ffa1ad18b1ed26c
Rajaf Dereje <rajafgudeta@gmail.com>
2025-09-19 16:15:41 +0300

    struct

--- Go/main.go
+++ Go/main.go
@@ -4,16 +4,7 @@ import "fmt"
 
 func main() {
 
-	menu := map[string]float64{"soup": 499, "pie": 799, "salad": 6.99, "tofee pudding": 5.55}
+	mybill := newBill("nario's bill")
 
-	fmt.Println(menu)
-	fmt.Println(menu["pie"])
-
-	//* looping maps
-	for key, value := range menu {
-		fmt.Println(key, "-", value)
-	}
-
-	//* update a map
-	menu["soup"] = 4.99
+	fmt.Println(mybill)
 }
